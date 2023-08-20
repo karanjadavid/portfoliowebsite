f41de5abce5980fb5944a94b196d65387fd3278c
David karanja <Dave@Karanja-MD.(none)>
2023-08-20 18:02:29 +0300

    Readjust video for phone orientation

--- web/src/components/Abilities.js
+++ web/src/components/Abilities.js
@@ -1,35 +1,31 @@
-import React, { useEffect } from "react";
-import Image from "../assets/o.jpg";
-import "./Abilities.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import VideoPlayer from 'react-video-js-player';
+import React, { useEffect } from 'react';
+import './Abilities.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Video from './videos/Elevator.mp4';
 import backgroundvideo from './videos/backgroundvideo.png';
 
 const Abilities = () => {
   const videoSrc = Video;
-  const poster = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/msnbc/Components/Photos/061107/061107_stevejobs_hmed_12p.jpg"
   useEffect(() => {
     AOS.init();
   }, []);
   return (
-    <div className="feature" id="about">
-      <div className="title-text" data-aos="zoom-in">
+    <div className='feature' id='about'>
+      <div className='title-text' data-aos='zoom-in'>
         <p>Who am I</p>
         <h1>About Me</h1>
       </div>
-      <div className="feature-box">
+      <div className='feature-box'>
         <div
-          className="features"
-          data-aos="fade-right"
-          data-aos-offset="300"
-          data-aos-easing="ease-in-sine"
-          data-aos-duration="800"
+          className='features'
+          data-aos='fade-right'
+          data-aos-offset='300'
+          data-aos-easing='ease-in-sine'
+          data-aos-duration='800'
         >
-          <div className="features-desc">
-           
-            <div className="feature-text">
+          <div className='features-desc'>
+            <div className='feature-text'>
               {/* <p>
                 I am a young developer currently residing in Nairobi, Kenya.I am
                 a diligent, hardworking and result oriented guy. I always work
@@ -39,11 +35,11 @@ const Abilities = () => {
           </div>
 
           <h1>Bio</h1>
-          <div className="features-desc">
-            <div className="feature-icon">
-              <i className="fab fa-black-tie"></i>
+          <div className='features-desc'>
+            <div className='feature-icon'>
+              <i className='fab fa-black-tie'></i>
             </div>
-            <div className="feature-text">
+            <div className='feature-text'>
               <p>
                 David Mwangi is a proficient Data Analyst at Mosmarts Limited, a
                 prominent software and data consultancy firm based in Nairobi,
@@ -62,14 +58,19 @@ const Abilities = () => {
           </div>
 
           <h1>Elevator Pitch</h1>
-          <div className='meetingpage'>  <div>
-          <VideoPlayer
-            src={videoSrc}
-            poster={backgroundvideo}
-            className='video'
-          />
-        </div>
-      </div>
+          <div className='meetingpage'>
+            <div>
+              <video
+                width='800'
+                height='550'
+                controls
+                className='video'
+                poster={backgroundvideo}
+              >
+                <source src={videoSrc} type='video/mp4' />
+              </video>
+            </div>
+          </div>
         </div>
       </div>
     </div>
